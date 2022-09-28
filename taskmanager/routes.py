ace8ade41760ec308dd2caffd598c0db6c548212
nfepb <nicolas.planteb@gmail.com>
2022-09-28 18:04:03 +0000

    Retrieves category name to be displayed when editing. Updates routes.py to enable POST method and commit changes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -29,6 +29,7 @@ def add_category():
 
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
+    # Query the db and attempt to find id
     category = Category.query.get_or_404(category_id)
     if request.method == "POST":
         category.category_name = request.form.get("category_name")
