34cfcfd1c724cd25a3e4fb1dfebbebc89cc5f9d3
nfepb <nicolas.planteb@gmail.com>
2022-09-30 16:04:44 +0000

    Adds modal and trigger for deleting in categories.html and script to launch modal in script.js

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Sidenav initialisation
-  var sidenav = document.querySelectorAll(".sidenav");
+  let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+  let modal = document.querySelectorAll(".modal");
+  M.Modal.init(modal);
 });
