bfa3c37c8268f89482c52822bdfb7646dfd70f56
nfepb <nicolas.planteb@gmail.com>
2022-09-30 14:24:27 +0000

    Updates routes.py to query categories based on id and delete

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -36,3 +36,12 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    # Query the db and attempt to find id
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
