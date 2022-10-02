c7d2639fc08738ff5d1f8cf21a02117dda96462e
nfepb <nicolas.planteb@gmail.com>
2022-10-02 09:44:33 +0000

    Adds form to create new task + map fields to db for commit. Adds script for checkbox, datepicker, and display list of category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   // Sidenav initialisation
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+  // Modal initialisation
   let modal = document.querySelectorAll(".modal");
   M.Modal.init(modal);
+  // Datepicker initialisation
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" },
+  });
+  // Select dropdown initialisation
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 });
